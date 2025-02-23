7297f0e8e94ac7b687ad8d28b19fcb06e4b02949
Titoxd0919 <angelzavaleta223@gmail.com>
2025-02-22 23:41:40 -0600

    Primer commit

---
+++ SessionVariable_19795391/SessionVariable.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionVariable_19795391
+{
+    public partial class SessionVariable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Session["ddlCategory"] = ddlCategory.SelectedValue;
+            Session["ddlSupplier"] = ddlSupplier.SelectedValue;
+            Session["strProduct"] = strProduct.Text;
+            Session["strDescription"] = strDescription.Text;
+            Session["strImage"] = strImage.Text;
+            Session["decPrice"] = decPrice.Text;
+            Session["bytNumberInStock"] = bytNumberInStock.Text;
+            Session["bytNumberOnOrder"] = bytNumberOnOrder.Text;
+            Session["bytReorderLevel"] = bytReorderLevel.Text;
+
+            Response.Redirect("SessionVariable2.aspx");
+        }
+    }
+}
